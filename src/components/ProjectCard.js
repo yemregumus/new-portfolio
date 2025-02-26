0757a7e5ccd26049bdb56edb2eba3ea926d42e6e
yemregumus <ygumus@myseneca.ca>
2025-02-25 21:34:11 -0500

    made progress on projects tab, added a project to test

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,4 +1,6 @@
-export const ProjectCard = ({ title, description, imageUrl }) => {
+import { Col, Button } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imageUrl, githubLink, linkToLive }) => {
   return (
     <Col sm={6} md={4} lg={3}>
       <div className="proj-imgbx">
@@ -6,6 +8,14 @@ export const ProjectCard = ({ title, description, imageUrl }) => {
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
+          <div className="proj-btns d-flex gap-2 mt-2">
+            <a href={githubLink} target="_blank" rel="noreferrer">
+              <Button variant="primary">GitHub</Button>
+            </a>
+            <a href={linkToLive} target="_blank" rel="noreferrer">
+              <Button variant="success">Live</Button>
+            </a>
+          </div>
         </div>
       </div>
     </Col>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,16 @@
-import { Col, Row, Nav, Tab } from "react-bootstrap";
-
-import { Container } from "react-bootstrap";
+import { Container, Col, Row, Nav, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
 
 export const Projects = () => {
   const projects = [
     {
-      title: "Project 1",
-      description: "Description 1",
-      imageUrl: "image1",
+      title: "The Met Site",
+      description: "The Metropolitan Museum of Art Site is built with React.js, Bootstrap, and Next.js for dynamic content delivery and server‐side rendering, the site leverages JWT for secure user authentication and authorization.",
+      imageUrl: projImg1,
+      githubLink: "https://github.com/yemregumus/WEB422-Assignment6",
+      linkToLive: "https://museumofartnewyork.netlify.app/",
     },
   ];
 
@@ -21,20 +24,20 @@ export const Projects = () => {
             <Tab.Container id="projects=tabs" defaultActiveKey="first">
               <Nav variant="pills" defaultActiveKey="/home">
                 <Nav.Item>
-                  <Nav.Link eventKey="first">Tab One </Nav.Link>
+                  <Nav.Link eventKey="first">Web Development</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                  <Nav.Link eventKey="second">Software Development</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                  <Nav.Link eventKey="third">Personal Projects</Nav.Link>
                 </Nav.Item>
               </Nav>
               <Tab.Content>
                 <Tab.Pane eventKey="first">
                   <Row>
                     {projects.map((project, index) => {
-                      return <p>{project.title}</p>;
+                      return <ProjectCard key={index} title={project.title} description={project.description} imageUrl={project.imageUrl} githubLink={project.githubLink} linkToLive={project.linkToLive} />;
                     })}
                   </Row>
                 </Tab.Pane>
@@ -45,6 +48,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
+      <img className="background-image-right" src={colorSharp2} alt="background" />
     </section>
   );
 };
