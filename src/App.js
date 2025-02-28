00a3643fdf62a8eb494b8b6ed5a2a53e880b0ef5
yemregumus <ygumus@myseneca.ca>
2025-02-27 22:04:00 -0500

    Contact component is done, nodemail server created - made some improvements in css and overall look

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Banner />
+
+      <BrowserRouter>
+        <Banner />
+      </BrowserRouter>
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,10 @@
 import { useState, useEffect, useMemo } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
+import { HashLink } from "react-router-hash-link";
 import headerImg from "../assets/img/header-img.svg";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
   const toRotate = useMemo(() => ["FullStack Developer", "React Developer", "Software Developer", "Web Developer", "Systems Designer"], []);
@@ -24,20 +27,34 @@ export const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portfolio</span>
-            <h1 className="title">{`I am Yunus Emre`}</h1>
-            <div className="txt-rotate">
-              <span className="wrap">A {text}</span>
-            </div>
-            <div>
-              <p className="desc">I am a Full-Stack Developer with a passion for creating beautiful and functional user experiences.</p>
-            </div>
-            <button onClick={() => console.log("connect pressed")} className="btn">
-              Let's Connect <ArrowRightCircle />
-            </button>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1 className="title">{`I am Yunus Emre`}</h1>
+                  <div className="txt-rotate">
+                    <span className="wrap">A {text}</span>
+                  </div>
+                  <div>
+                    <p className="desc">I am a Full-Stack Developer with a passion for creating beautiful and functional user experiences.</p>
+                  </div>
+                  <HashLink smooth to="#connect" className="text-decoration-none">
+                    <button>
+                      Let's Connect <ArrowRightCircle size={21} />
+                    </button>
+                  </HashLink>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header Img" />
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
