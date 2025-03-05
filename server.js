30428497618584c0d10719ccedc044d719dc17ca
yemregumus <ygumus@myseneca.ca>
2025-03-04 21:12:17 -0500

    added dynamic port to server

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 
+const PORT = process.env.PORT || 5000;
+
 //server used to send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
@@ -24,6 +26,10 @@ contactEmail.verify((error) => {
   } else {
     console.log("Ready to Send");
   }
+});
+
+router.get("/", (req, res) => {
+  res.send("Server is running!");
 });
 
 router.post("/contact", (req, res) => {
